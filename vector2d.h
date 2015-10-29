3fdae9cf953d971c207b9bbdecbd3a58a2a5a83f
Mikle-Bond <Mikle.Bond.1997@gmail.com>
2015-10-30 01:36:24 +0300

    extended Vector2D struct
    Now we can add it, mul it and norm it.

--- vector2d.h
+++ vector2d.h
@@ -8,6 +8,17 @@ struct Vector2D
     double y;
     explicit Vector2D(double _x = 0.0, double _y = 0.0);
     ~Vector2D();
+
+    Vector2D &operator +=(const Vector2D &rhs);
+    Vector2D &operator *=(double a);
+
+    // make this vector normal
+    Vector2D &normalize(void);
+
+    // build a normal copy of *this
+    Vector2D normal(void);
+
+//    static Vector2D unit(double phi); -- do we need it?
 };
 
 #endif // VECTOR2D_H
