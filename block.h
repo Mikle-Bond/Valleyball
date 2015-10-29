b30b01e44ce4d62e5ca4b2ff061e8290ed6798b9
Mikle-Bond <Mikle.Bond.1997@gmail.com>
2015-10-30 00:42:08 +0300

    added Block struct
    Will be in use for all static objects on game field.

---
+++ block.h
@@ -0,0 +1,24 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+/* Here block means a flat plane in 2D (line, to be honest).
+ * That's why here we need to know only two points to handle it.
+ * Vectors to the ends are builded from the (0,0) point, or from the container
+ * position, if there will be some.
+ */
+
+#include "vector2d.h"
+
+struct Block
+{
+    Vector2D a, b;
+    explicit Block(
+            double ax = 0.0,
+            double ay = 0.0,
+            double bx = 0.0,
+            double by = 0.0);
+    Block (const Vector2D &_a, const Vector2D &_b);
+    // Default copy constructor is just fine.
+    ~Block();
+};
+
+#endif // BLOCK_H
