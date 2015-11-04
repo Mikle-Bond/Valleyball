bfc29b70d9a21bf79bc1a751c40d98df28dd90f3
Mikle-Bond <Mikle.Bond.1997@gmail.com>
2015-11-04 12:15:54 +0300

    added Player class
    Here is scrtch variant of base class for players.

--- player.cpp
+++ player.cpp
@@ -1,6 +1,11 @@
 #include "player.h"
 
-Player::Player(Vector2D left, Vector2D right, double height, double max_speed, double max_force, double curr)
+Player::Player(Vector2D &left, Vector2D &right, double height, 
+	double max_speed, double max_force, double curr) 
+:
+	pos(left, right, height, curr),
+	max_force_(max_force),
+	max_speed_(max_speed)
 {
 
 }
@@ -10,3 +15,23 @@ Player::~Player()
 
 }
 
+Player::position::position(Vector2D &left, Vector2D &right, 
+	double height, double position)
+:
+	lt_edge_(left),
+	rt_edge_(right),
+	height_(height),
+	pos_mul_(position)
+{
+
+}
+
+Vector2D Player::position::get_pos()
+{
+	return (rt_edge_ - lt_edge_) * pos_mul_ + lt_edge_;
+}
+
+Vector2D Player::position::get_head()
+{
+	return get_pos() + Vector2D(0.0, height_);
+}
--- player.h
+++ player.h
@@ -6,33 +6,34 @@
 
 class Player
 {
-    double max_force;
-    double max_speed;
 public:
-    Player(
-            Vector2D left,
-            Vector2D right,
-            double height,
-            double max_speed,
-            double max_force,
-            double curr = 0.5);
-    ~Player();
+	class position : public Block
+	{
+	public:
+		position(Vector2D &left, Vector2D &right, 
+			double height, double position);
+		Vector2D get_pos(void);
+		Vector2D get_head(void);
+		// here we understend position as
+		// lt_edge + (rt_edge - lt_edge) * pos_mul;
+	private:
+		Vector2D lt_edge_;
+		Vector2D rt_edge_;
+		double height_;
+		double pos_mul_;
+	};
 
-    class pos : public Block
-    {
-        Vector2D lt_edge;
-        Vector2D rt_edge;
-        double pos_mul;
-        double height;
-        // here we understend position as
-        // lt_edge + (rt_edge - lt_edge) * pos_mul;
-    public:
-        Vector2D get_pos(void);
-        Vector2D get_head(void);
-    };
+	Player(Vector2D &left, Vector2D &right, double height,
+		double max_speed, double max_force, double curr = 0.5);
+	~Player();
 
-    virtual void idle(void);
-    virtual Vector2D get_force();
+	virtual void idle(void);
+	virtual Vector2D get_force(void);
+
+	position pos;
+private:
+	double max_force_;
+	double max_speed_;
 };
 
 #endif // PLAYER_H
