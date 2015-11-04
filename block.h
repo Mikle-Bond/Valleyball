c9be6445c517b63b14c99b46c6f671b39db63e78
Mikle-Bond <Mikle.Bond.1997@gmail.com>
2015-11-04 15:27:23 +0300

    cosmetic commit for Block struct
    Just to make all files written in single codestyle.

--- block.h
+++ block.h
@@ -10,15 +10,16 @@
 
 struct Block
 {
-    Vector2D a, b;
-    explicit Block(
-            double ax = 0.0,
-            double ay = 0.0,
-            double bx = 0.0,
-            double by = 0.0);
-    Block (const Vector2D &_a, const Vector2D &_b);
-    // Default copy constructor is just fine.
-    ~Block();
+	explicit Block(
+		double ax = 0.0,
+		double ay = 0.0,
+		double bx = 0.0,
+		double by = 0.0);
+	Block (const Vector2D &_a, const Vector2D &_b);
+	// Default copy constructor is just fine.
+	~Block();
+
+	Vector2D a, b;
 };
 
 #endif // BLOCK_H
