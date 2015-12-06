1f06739e3b762c6262c2b95a6d19c5142d4e6cdf
Orleg <krenshenebon@mail.ru>
2015-12-06 21:15:11 +0300

    class bool realize in movable context

--- ball.h
+++ ball.h
@@ -2,81 +2,64 @@
 #define BALL
 #include "vector2d.h"
 #include "block.h"
+#include "movable.h"
 
 #define NONE_CROSS       0
 #define VERTICAL_CROSS   1
-#define HORIZONTAL_CRASS 2
+#define HORIZONTAL_CROSS 2
 #define SLOPPING_CROSS   3
 
-#if 0
-struct Force        //is it usefull????
+class Ball : public Movable
 {
-    Vector2D F;
-
-    Force(double fx = 0, double fy = 0):F(fx, fy)
-    {
-    }
-#if 0
-    double value;
-    double angle;//from horisont
-    Force(double val = 0, double ang = 0)
-    {
-        value = val;
-        angle = ang;
-    }
-    //may be (double *F) and create array of 2 elements: Fx and Fy - can be changig for 3D easier
-#endif
-};
-#endif
-class Ball
-{
-    Vector2D coordinates;
-    Vector2D velocity;
-    Force force;//external force, if i need it here???
+    Vector2D acceleration;//by external force, if i need it here???
     double _mass;
     double _Radius;
 
 public:
-    Ball(double x = 0, double y = 0, double vx = 0, double vy = 0,\
-         double xforce = 0,double yforce = 9.81, double mass = 1, double radius = 0.1)\
-        :coordinates(x, y),velocity(vx, vy),force(xforce, yforce),_mass(mass),_Radius(radius)
+    Vector2D X0Y0;
+//-------------------------------------------------------------------------------------------------
+    Ball(const Vector2D &left, const Vector2D &right,
+         const Vector2D &st_position, const Vector2D &st_speed,\
+         Vector2D accel = Vector2D(0, 0.0981), double mass = 1, double radius = 0.01)\
+        :Movable(left, right, st_position, st_speed), X0Y0(st_position),acceleration(accel)\
+        ,_mass(mass),_Radius(radius)
     {
+    }
+    ~Ball(){}
+//-------------------------------------------------------------------------------------------------
+    inline Vector2D get_speed() const
+    {
+        return speed;
+    }
+    inline Vector2D get_accel()
+    {
+        return acceleration;
     }
 //-------------------------------------------------------------------------------------------------
-    double get_x() const
+    inline double get_Radius() const
     {
-        return coordinates.x;
-    }
-
-    double get_y() const
-    {
-        return coordinates.y;
-    }
-
-    double get_Vx() const
-    {
-        return velocity.x;
-    }
-
-    double get_Vy() const
-    {
-        return velocity.y;
+        return _Radius;
     }
 //-------------------------------------------------------------------------------------------------
-    void push(Force f, double dt)
+    inline double get_Mass()
     {
-        f.F+=this->force.F;
-        velocity.x += (f.F.x * dt) / mass;
-        velocity.y += (f.F.y * dt) / mass;
+        return _mass;
     }
 //-------------------------------------------------------------------------------------------------
-    void next_step(double dt)
+    void push(Vector2D f, double dt)
     {
-        coordinates.x += vx * dt;
-        coordinates.y += vy * dt;
+        speed.x += (f.x / _mass + acceleration.x) * dt;
+        speed.y += (f.y / _mass + acceleration.y) * dt;
+    }
+//-------------------------------------------------------------------------------------------------
 
-        velocity.x += (this->force.F.x) * dt / mass;
-        velocity.y += (this->force.F.y) * dt / mass;
+//-------------------------------------------------------------------------------------------------
+    bool move(double dt) override
+    {
+        speed.x += (acceleration.x) * dt;
+        speed.y += (acceleration.y) * dt;
+
+        return Movable::move(dt);// may be it isn't need becose of we haven't crossing with walls
     }
 /*
  * now only vertical and horizontal blocks, may be sloping in the future
@@ -85,12 +68,13 @@ public:
     int IsCrossing (const Block &block)//i hope that block is orientated
     {
         if (block.a.x == block.b.x)
-            if(((velocity.x > 0) &&  (coordinates.x >= block.a.x))
-            || ((velocity.x < 0) &&  (coordinates.x <= block.a.x))) return VERTICAL_CROSS;
+            if(((speed.x > 0) &&  (get_pos().x >= block.a.x))
+            || ((speed.x < 0) &&  (get_pos().x <= block.a.x))) return VERTICAL_CROSS;
 
         if (block.a.y == block.b.y)
-            if(((velocity.y > 0) &&  (coordinates.y >= block.a.y))
-            || ((velocity.y < 0) &&  (coordinates.y <= block.a.y))) return VERTICAL_CROSS;
+            if(((speed.y > 0) &&  (get_pos().y >= block.a.y))
+            || ((speed.y < 0) &&  (get_pos().y <= block.a.y))) return HORIZONTAL_CROSS;
+        return NONE_CROSS;
     }
 
 };
