4f64f132a4af090fd2c6bb0812bb853371d6edd0
Orleg <krenshenebon@mail.ru>
2015-12-09 08:24:33 +0300

    Idle
    add idle in Sam

--- sam.h
+++ sam.h
@@ -11,12 +11,13 @@ class SamThePlayer : public Player
     bool side;
 
     double were(Ball& ball);
-    Vector2D How(Manager &mng);
+    Vector2D How();
 
 public:
     SamThePlayer(const Vector2D &left, const Vector2D &right, bool Side);
 	~SamThePlayer();
     Vector2D get_force(Manager &Mng);
+    virtual bool move(double dt);
 	virtual void idle(void);
     virtual Vector2D push(void);
 };
