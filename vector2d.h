60640a0844e7db35cfd05ad709ba1dbabe1c0ed4
Mikle-Bond <Mikle_Bond@mail.ru>
2015-12-19 23:18:46 +0300

    Adding missed functionality to the Vector2D class.
    Now, dividing by number is avaluable.

--- vector2d.h
+++ vector2d.h
@@ -4,22 +4,23 @@
 
 struct Vector2D
 {
-    explicit Vector2D(double _x = 0.0, double _y = 0.0);
-    virtual ~Vector2D();
+	explicit Vector2D(double _x = 0.0, double _y = 0.0);
+	virtual ~Vector2D();
 
-    Vector2D &operator +=(const Vector2D &rhs);
-    Vector2D &operator -=(const Vector2D &rhs);
-    Vector2D &operator *=(double a);
+	Vector2D &operator +=(const Vector2D &rhs);
+	Vector2D &operator -=(const Vector2D &rhs);
+	Vector2D &operator *=(double a);
+	Vector2D &operator /=(double a);
 	double operator ()(const Vector2D &rhs);
 
-    // make this vector normal
-    Vector2D &normalize(void);
+	// make this vector normal
+	Vector2D &normalize(void);
 
-    // build a normal copy of *this
-    Vector2D normal(void);
+	// build a normal copy of *this
+	Vector2D normal(void);
 
-    double x;
-    double y;
+	double x;
+	double y;
 //    static Vector2D unit(double phi); -- do we need it?
 };
 
@@ -27,8 +28,10 @@ Vector2D operator +(const Vector2D &lt, const Vector2D &rt);
 Vector2D operator -(const Vector2D &lt, const Vector2D &rt);
 Vector2D operator *(const Vector2D &lt, double a);
 Vector2D operator *(double a, const Vector2D &rt);
+Vector2D operator /(const Vector2D &lt, double a);
 
 bool operator==(const Vector2D &lt, const Vector2D &rt);
+bool operator!=(const Vector2D &lt, const Vector2D &rt);
 
 #endif // VECTOR2D_H
 
